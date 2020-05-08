f68f14d196e7cf4aa5a9d96fcad85db489e91782
Q-ro <andres.mrad@gmail.com>
2020-05-07 19:25:52 -0500

    Updated main menu

---
+++ Gomoku GSN Test/Assets/_Scripts/UI/SceneTransitionAnimator.cs
@@ -0,0 +1,35 @@
+﻿/*
+Author: Andres Mrad (Q-ro)
+Creation Date : [ 2020/05 (May)/06 (Wed) ] @[ 16:11 ]
+Description :  A general purpose helper to perform simple scene transition animations
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SceneTransitionAnimator : MonoBehaviour
+{
+    #region Inspector Properties
+
+        [SerializeField] Image transitionMask;
+
+    #endregion
+    
+    #region Private Properties
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
